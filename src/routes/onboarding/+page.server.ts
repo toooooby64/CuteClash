9ccee13b1173328dfbad88306761ec03872cd344
toooooby64 <jalvarez5667@gmail.com>
2024-01-10 23:40:01 -0800

    Created better errors messages for uploading photos

--- src/routes/onboarding/+page.server.ts
+++ src/routes/onboarding/+page.server.ts
@@ -5,18 +5,32 @@ export const actions: Actions = {
 		const test = await locals.getSession();
 		const formData = await request.formData();
 		const errors = [];
+		const totalPhotos = Array.from(formData.entries()).length;
+
 		for (const [key, value] of formData.entries()) {
 			const { data, error } = await locals.supabase.storage
-				.from('user-photo')
+				.from('user-photos')
 				.upload(`${test.user.id}/${value.name}`, value);
 			if (error) {
-				errors.push(
-					`There was a error uploading ${value.name} please try again later.`
-				);
+				console.log(value);
+				errors.push(value.name);
+				console.log(errors);
 			}
 		}
+		let totalErrors: number = errors.length;
+
 		if (errors.length > 0) {
-			return fail(500, { errors });
+			if (totalErrors === totalPhotos) {
+				return fail(500, {
+					message: 'There was a problem uploading your photos. Please try again.',
+					sucessfulUploads: totalPhotos - totalErrors
+				});
+			}
+			return fail(500, {
+				message: `${totalErrors}/${totalPhotos} photos failed to upload. \n There was a problem with ${errors} please try again.`,
+				sucessfulUploads: totalPhotos - totalErrors
+			});
 		}
+		return { success: true, sucessfulUploads: totalPhotos - totalErrors };
 	}
 };
--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -8,6 +8,9 @@
 	import Layout from '../ui/+layout.svelte';
 	import { Toast, getToastStore } from '@skeletonlabs/skeleton';
 	import type { ToastSettings, ToastStore } from '@skeletonlabs/skeleton';
+
+	$: successfulUploads = 0;
+
 	const toastStore = getToastStore();
 	const createToast = (message: string) => {
 		const t: ToastSettings = {
@@ -33,16 +36,22 @@
 
 	const uploadPhoto = async (event) => {
 		const formData = new FormData(event.target);
+		const totalPhotos = Array.from(formData.entries()).length;
+		console.log('this?')
+		console.log(totalPhotos);
+		if (totalPhotos === 0) {
+			return;
+		}
 		const response = await fetch(event.target.action, { method: 'POST', body: formData });
 
 		const res = await response.json();
 		const data = JSON.parse(res.data);
-		console.log(data)
-		if (data[2]) {
-			for (let i = 2; i < data.length; i++) {
-				createToast(data[i]);
-			}
-			
+		if (data[1]) {
+			createToast(data[1]);
+			successfulUploads += data[2];
+		} else {
+			createToast('Photos uploaded successfully');
+			successfulUploads += data[2];
 		}
 	};
 </script>
@@ -54,28 +63,36 @@
 				<form action="?/uploadPhoto" method="post" on:submit|preventDefault={uploadPhoto}>
 					<Step buttonNextType="submit">
 						<svelte:fragment slot="header">Step 1: Upload pictures of your pet!</svelte:fragment>
-						<div class="flex flex-col justify-center">
-							{#if numOfFileInputs < 5}
-								<button type="button" on:click={addPhoto} class="btn variant-filled-primary"
-									>Add photo</button
-								>
-							{:else}
-								<button type="button" class="btn variant-filled-warning" disabled>Add photo</button>
-								{createToast('Only 5 photos are allowed')}
-							{/if}
+						{#if successfulUploads === 5}
+							<div class="flex justify-center">
+								<h2 class="h2">You have uploaded 5 photos.</h2>
+							</div>
+						{:else}
+							<div class="flex flex-col justify-center">
+								{#if numOfFileInputs < 5}
+									<button type="button" on:click={addPhoto} class="btn variant-filled-primary"
+										>Add photo</button
+									>
+								{:else}
+									<button type="button" class="btn variant-filled-warning" disabled
+										>Add photo</button
+									>
+									{createToast('Only 5 photos are allowed')}
+								{/if}
 
-							{#each fileInputs as _, index}
-								<div class="flex justify-between">
-									<input
-										class=" m-1 w-1/2 input"
-										type="file"
-										id={`file-${index}`}
-										name={`file-${index}`}
-									/>
-									<button on:click={removePhoto}>X</button>
-								</div>
-							{/each}
-						</div>
+								{#each fileInputs as _, index}
+									<div class="flex justify-between">
+										<input
+											class=" m-1 w-1/2 input"
+											type="file"
+											id={`file-${index}`}
+											name={`file-${index}`}
+										/>
+										<button type="button" on:click={() => removePhoto(index)}>X</button>
+									</div>
+								{/each}
+							</div>
+						{/if}
 					</Step>
 				</form>
 
